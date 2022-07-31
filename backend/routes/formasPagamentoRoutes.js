4f5f02fa6497dabf6faf8a45dccb810b8d1fb816
Marcio Junior <marcio_ale1@hotmail.com>
2022-07-31 00:46:24 -0300

    Routes Formas Pagamento

---
+++ backend/routes/formasPagamentoRoutes.js
@@ -0,0 +1,34 @@
+const ctrlFormasPagamento = require('../controllers/ctrlFormasPagamento');
+
+function formaspagamentoRoutes(req, res) {
+    if(req.method === 'OPTIONS') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end();
+    } else if(req.method === 'GET') {
+        if(req.url.match(/\/api\/formaspagamento\/([0-9+])/)) {
+            const id = req.url.split('/')[3];
+            ctrlFormasPagamento.buscarUm(req, res, id);
+        } else if (req.url.includes('filter')) {
+            ctrlFormasPagamento.validate(req, res);
+        } else if (req.url.includes('page=all')) {
+            ctrlFormasPagamento.buscarTodosSemPg(req, res)
+        } else {
+            ctrlFormasPagamento.buscarTodosComPg(req, res);
+        }
+    } else if(req.url === '/api/formaspagamento' && req.method === 'POST') {
+        ctrlFormasPagamento.salvar(req, res);
+    } else if(req.url.match(/\/api\/formaspagamento\/([0-9+])/) && req.method === 'PUT') {
+        const id = req.url.split('/')[3];
+        ctrlFormasPagamento.alterar(req, res, id);
+    } else if(req.url.match(/\/api\/formaspagamento\/([0-9+])/) && req.method === 'DELETE') {
+        const id = req.url.split('/')[3];
+        ctrlFormasPagamento.deletar(req, res, id);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Rota não encontrada.'}));
+    }
+}
+
+module.exports = {
+    formaspagamentoRoutes
+}
