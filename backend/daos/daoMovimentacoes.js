cba411f654210c55dff099e1f434356a983a145a
Marcio Junior <marcio_ale1@hotmail.com>
2022-12-07 00:26:09 -0300

    daoMovimentacoes

--- backend/daos/daoMovimentacoes.js
+++ backend/daos/daoMovimentacoes.js
@@ -1,4 +1,6 @@
 const { pool } = require('../datamodule/index');
+const daoClientes = require('./daoClientes');
+const daoFornecedores = require('./daoFornecedores');
 
 // @descricao BUSCA TODOS OS REGISTROS
 // @route GET /api/grades
@@ -20,20 +22,76 @@ async function buscarTodosComPg (url) {
     page = page.replace(/[^0-9]/g, '');
     return new Promise((resolve, reject) => {
         if (url.endsWith('=')) {
-            pool.query(`select * from grades order by id asc limit ${limit} offset ${(limit*page)-limit}`,(err, res) => {
+            pool.query(`select * from movimentacoes order by id asc limit ${limit} offset ${(limit*page)-limit}`, async (err, res) => {
                 if (err) {
                     return reject(err);
                 }
-                return resolve(res.rows);
+                const mListaMovimentacoes = [];
+                for (let i = 0; i < res.rows.length; i++) {
+                    let movimentacao = res.rows[i];
+                    let pessoa = {};
+                    if (movimentacao.tipo == 'E') {
+                        let response = await daoClientes.buscarUm(movimentacao.idpessoa);
+                        pessoa = response.rows[0];
+                        mListaMovimentacoes.push({
+                            id: movimentacao.id,
+                            tipo: movimentacao.tipo,
+                            dtmovimentacao: movimentacao.dtmovimentacao,
+                            valor: movimentacao.valor,
+                            pessoa: pessoa
+                        })
+                    } else {
+                        let response = await daoFornecedores.buscarUm(movimentacao.idpessoa);
+                        pessoa = response.rows[0];
+                        mListaMovimentacoes.push({
+                            id: movimentacao.id,
+                            tipo: movimentacao.tipo,
+                            dtmovimentacao: movimentacao.dtmovimentacao,
+                            valor: movimentacao.valor,
+                            pessoa: pessoa
+                        })
+                    }
+                }
+                return resolve(mListaMovimentacoes);
             })
         } else {
             var filter = url.split('=')[3];
-            pool.query(`select * from grades where descricao like '%${filter.toUpperCase()}%' limit ${limit} offset ${(limit*page)-limit}`, (err, res) => {
+            if (filter.includes('ENTRADA')) {
+                filter = 'E';
+            } else if (filter.includes('SAIDA')) {
+                filter = 'S';
+            }
+            pool.query(`select * from movimentacoes where tipo = '${filter}' limit ${limit} offset ${(limit*page)-limit}`, (err, res) => {
                 if (err) {
                     return reject(err);
                 }
-                console.log(res);
-                return resolve(res.rows);
+                const mListaMovimentacoes = [];
+                for (let i = 0; i < res.rows.length; i++) {
+                    let movimentacao = res.rows[i];
+                    let pessoa = {};
+                    if (movimentacao.tipo == 'E') {
+                        let response = await daoClientes.buscarUm(movimentacao.idpessoa);
+                        pessoa = response.rows[0];
+                        mListaMovimentacoes.push({
+                            id: movimentacao.id,
+                            tipo: movimentacao.tipo,
+                            dtmovimentacao: movimentacao.dtmovimentacao,
+                            valor: movimentacao.valor,
+                            pessoa: pessoa
+                        })
+                    } else {
+                        let response = await daoFornecedores.buscarUm(movimentacao.idpessoa);
+                        pessoa = response.rows[0];
+                        mListaMovimentacoes.push({
+                            id: movimentacao.id,
+                            tipo: movimentacao.tipo,
+                            dtmovimentacao: movimentacao.dtmovimentacao,
+                            valor: movimentacao.valor,
+                            pessoa: pessoa
+                        })
+                    }
+                }
+                return resolve(mListaMovimentacoes);
             })
         }
     })
